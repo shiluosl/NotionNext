a80a1a49277070088c4caabfc52f141c13c567d8
shiluosl <113872027+shiluosl@users.noreply.github.com>
2025-05-27 16:05:30 +0800

    111

--- conf/code.config.js
+++ conf/code.config.js
@@ -20,7 +20,7 @@ module.exports = {
     process.env.NEXT_PUBLIC_PRISM_THEME_DARK_PATH ||
     'https://raw.githubusercontent.com/PrismJS/prism-themes/refs/heads/master/themes/prism-material-dark.css', // 深色模式主题
 
-  CODE_MAC_BAR: process.env.NEXT_PUBLIC_CODE_MAC_BAR || false, // 代码左上角显示mac的红黄绿图标
+  CODE_MAC_BAR: process.env.NEXT_PUBLIC_CODE_MAC_BAR || true, // 代码左上角显示mac的红黄绿图标
   CODE_LINE_NUMBERS: process.env.NEXT_PUBLIC_CODE_LINE_NUMBERS || true, // 是否显示行号
   CODE_COLLAPSE: process.env.NEXT_PUBLIC_CODE_COLLAPSE || true, // 是否支持折叠代码框
   CODE_COLLAPSE_EXPAND_DEFAULT:
