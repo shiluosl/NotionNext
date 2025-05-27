bbc08e6ec9516517a82a05837b0bb6266d64c95a
shiluosl <113872027+shiluosl@users.noreply.github.com>
2025-05-27 10:43:34 +0800

    更新

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,6 +20,9 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
+  HERO_TITLE_3: '',
+  HERO_TITLE_4: '新版上线',
+  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
   HEO_HERO_TITLE_LINK: 'https://blog.shiluosl.top',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
