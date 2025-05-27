e36728269baaab2100fd996ff27e64eb4e0852a9
shiluosl <113872027+shiluosl@users.noreply.github.com>
2025-05-27 16:02:27 +0800

    22

--- conf/code.config.js
+++ conf/code.config.js
@@ -11,16 +11,16 @@ module.exports = {
   // 代码主题 @see https://github.com/PrismJS/prism-themes
   PRISM_THEME_PREFIX_PATH:
     process.env.NEXT_PUBLIC_PRISM_THEME_PREFIX_PATH ||
-    'https://npm.elemecdn.com/prism-themes/themes/prism-vsc-dark-plus.css', // 代码块默认主题
+    'https://raw.githubusercontent.com/PrismJS/prism-themes/refs/heads/master/themes/prism-material-light.css', // 代码块默认主题
   PRISM_THEME_SWITCH: process.env.NEXT_PUBLIC_PRISM_THEME_SWITCH || true, // 是否开启浅色/深色模式代码主题切换； 开启后将显示以下两个主题
   PRISM_THEME_LIGHT_PATH:
     process.env.NEXT_PUBLIC_PRISM_THEME_LIGHT_PATH ||
-    'https://npm.elemecdn.com/prism-themes/themes/prism-vsc-dark-plus.css', // 浅色模式主题
+    'https://raw.githubusercontent.com/PrismJS/prism-themes/refs/heads/master/themes/prism-material-light.css', // 浅色模式主题
   PRISM_THEME_DARK_PATH:
     process.env.NEXT_PUBLIC_PRISM_THEME_DARK_PATH ||
-    'https://npm.elemecdn.com/prism-themes/themes/prism-vsc-dark-plus.css', // 深色模式主题
+    'https://raw.githubusercontent.com/PrismJS/prism-themes/refs/heads/master/themes/prism-material-dark.css', // 深色模式主题
 
-  CODE_MAC_BAR: process.env.NEXT_PUBLIC_CODE_MAC_BAR || true, // 代码左上角显示mac的红黄绿图标
+  CODE_MAC_BAR: process.env.NEXT_PUBLIC_CODE_MAC_BAR || false, // 代码左上角显示mac的红黄绿图标
   CODE_LINE_NUMBERS: process.env.NEXT_PUBLIC_CODE_LINE_NUMBERS || true, // 是否显示行号
   CODE_COLLAPSE: process.env.NEXT_PUBLIC_CODE_COLLAPSE || true, // 是否支持折叠代码框
   CODE_COLLAPSE_EXPAND_DEFAULT:
